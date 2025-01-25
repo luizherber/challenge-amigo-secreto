f6f50edbb1b8ceaa67fbadc341b1193d47b140a1
Luiz Herber <luizherber@yahoo.com.br>
2025-01-25 12:15:42 -0300

    criada função que adiciona amigos

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let listaAmigos = [];
+
+function adicionarAmigo() {
+    let amigo = document.getElementById('amigo').value;
+    console.log(amigo);
+    if (amigo == '') {
+        alert('Por favor, insira um nome.')
+    } else {
+        listaAmigos.push(amigo);
+        amigo = document.getElementById('amigo');
+        amigo.value = '';
+    }
+    alert(listaAmigos);
+}
+
